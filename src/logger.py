c39de4f4aca3572f0db155c939dc8a2334384c96
Shubhanshu-G <subanshugupta135@gmail.com>
2026-07-24 23:19:53 +0530

    first commit

--- src/logger.py
+++ src/logger.py
@@ -22,3 +22,59 @@ if __name__=="__main__":
 
 
 print("Logging done")
+
+# import csv
+# import os
+# from datetime import datetime
+
+# # Create Logs folder
+# LOG_DIR = "logs"
+# os.makedirs(LOG_DIR, exist_ok=True)
+
+# # CSV file
+# LOG_FILE = os.path.join(LOG_DIR, "application_log.csv")
+
+# # CSV Header
+# HEADER = [
+#     "Timestamp",
+#     "Level",
+#     "Module",
+#     "Function",
+#     "Line No",
+#     "Message"
+# ]
+
+# # Create file with header if it doesn't exist
+# if not os.path.exists(LOG_FILE):
+#     with open(LOG_FILE, "w", newline="", encoding="utf-8") as file:
+#         writer = csv.writer(file)
+#         writer.writerow(HEADER)
+
+
+# def log(level, module, function, line_no, message):
+#     """Write log information into CSV."""
+
+#     with open(LOG_FILE, "a", newline="", encoding="utf-8") as file:
+#         writer = csv.writer(file)
+
+#         writer.writerow([
+#             datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+#             level,
+#             module,
+#             function,
+#             line_no,
+#             message
+#         ])
+
+
+# if __name__ == "__main__":
+
+#     log(
+#         level="INFO",
+#         module=__name__,
+#         function="main",
+#         line_no=42,
+#         message="Logging has started"
+#     )
+
+#     print("Logging Done")
