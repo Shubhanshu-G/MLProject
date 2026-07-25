b733567e1fa1070cb069a47c2273e663297be505
Shubhanshu-G <subanshugupta135@gmail.com>
2026-07-25 22:43:33 +0530

    For Deployment

--- src/logger.py
+++ src/logger.py
@@ -1,17 +1,42 @@
+# import logging
+# import os
+# from datetime import datetime
+
+# LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+# os.makedirs(logs_path,exist_ok=True)
+
+# LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+# logging.basicConfig(
+#     filename=LOG_FILE_PATH,
+#     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+#     level=logging.INFO,
+
+
+# )
 import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# Vercel (read-only filesystem)
+if os.getenv("VERCEL"):
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    )
 
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+# Local / AWS / Render
+else:
+    logs_path = os.path.join(os.getcwd(), "logs")
+    os.makedirs(logs_path, exist_ok=True)
 
+    LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
-)
+    logging.basicConfig(
+        filename=LOG_FILE_PATH,
+        format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+    )
